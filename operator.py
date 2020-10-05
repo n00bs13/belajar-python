d859ecaa7b629374608dcdf977900f6803abd3fc
root <root@localhost.localdomain>
2020-10-05 10:41:35 -0400

    first commit

---
+++ operator.py
@@ -0,0 +1,23 @@
+a = input("input nilai a : ")
+b = input("input nilai b : ")
+
+penambahan = a + b
+perkalian = a * b
+pembagian = a / b
+sisa_bagi = a % b
+pangkat = a ** b
+
+print "===== PENAMBAHAN ====="
+print "hasil %d + %d adalah %d " % (a,b,penambahan)
+
+print "===== PENAMBAHAN ====="
+print "hasil %d x %d adalah %d " % (a,b,perkalian)
+
+print "===== PEMBAGIAN ====="
+print "hasil %d : %d adalah %d " % (a,b,pembagian)
+
+print "===== SISA BAGI ====="
+print "hasil %d %% %d adalah %d " % (a,b,sisa_bagi)
+
+print "===== PANGKAT ====="
+print "hasil %d ** %d adalah %d " % (a,b,pangkat)
---
+++ penugasan.py
@@ -0,0 +1,7 @@
+a = input("masukan angka : ")
+
+print "nilai a sekarang %d" % a
+
+a +=5
+
+print "setelah ditambah operator penugasan %d " % a
